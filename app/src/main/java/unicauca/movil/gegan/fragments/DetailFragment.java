9e2d2772432e84cb71009694f8c6780ba6de9490
José Luis Beltrán <jlbeltran94@gmail.com>
2016-12-15 11:54:34 -0500

    iniciando con las tabs :v

---
+++ app/src/main/java/unicauca/movil/gegan/fragments/DetailFragment.java
@@ -0,0 +1,19 @@
+package unicauca.movil.gegan.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by jlbel on 15/12/2016.
+ */
+
+public class DetailFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
