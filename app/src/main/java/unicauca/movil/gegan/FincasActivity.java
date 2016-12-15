dbb2a39e0081e9e0434aa482fe17dff6682ab0e9
José Luis Beltrán <jlbeltran94@gmail.com>
2016-12-15 12:44:26 -0500

    finca detail activity in progress

---
+++ app/src/main/java/unicauca/movil/gegan/FincasActivity.java
@@ -0,0 +1,17 @@
+package unicauca.movil.gegan;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jlbel on 15/12/2016.
+ */
+
+public class FincasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
