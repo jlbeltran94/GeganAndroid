0a9d0052ba8ba5b5dd277b785c6d78726f7a4c84
José Luis Beltrán <jlbeltran94@gmail.com>
2016-12-15 12:44:53 -0500

    finca detail activity in progress 2 :v

--- app/src/main/java/unicauca/movil/gegan/FincasActivity.java
+++ app/src/main/java/unicauca/movil/gegan/FincasActivity.java
@@ -10,8 +10,12 @@ import android.support.v7.app.AppCompatActivity;
 
 public class FincasActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
 }
--- app/src/main/java/unicauca/movil/gegan/fragments/DetailFragment.java
+++ app/src/main/java/unicauca/movil/gegan/fragments/DetailFragment.java
@@ -3,17 +3,33 @@ package unicauca.movil.gegan.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+
+import unicauca.movil.gegan.database.FincaDao;
+import unicauca.movil.gegan.models.Finca;
 
 /**
  * Created by jlbel on 15/12/2016.
  */
 
-public class DetailFragment extends Fragment {
+public class DetailFragment extends FragmentActivity {
+
+    public static final String EXTRA_IDFINCA = "idfinca";
+    FincaDao dao;
+    Finca finca;
+    Long idfinca;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        idfinca = getIntent().getExtras().getLong(EXTRA_IDFINCA);
+        dao = new FincaDao(this);
+        finca = dao.getById(idfinca);
+
 
     }
+
+
 }
